a601fa0ddf8f80f1baf8a050a8da9b7256435bfb
David Knott <davek@communicraft.com>
2020-12-09 09:38:09 +0000

    integrated compiler libs into web-backend

--- web-backend/src/main/java/hello/Application.java
+++ web-backend/src/main/java/hello/Application.java
@@ -6,12 +6,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chaosopher.tigerlang.compiler.sugar.Desugar;
+import com.chaosopher.tigerlang.compiler.absyn.Absyn;
+import com.chaosopher.tigerlang.compiler.absyn.PrettyPrinter;
+import com.chaosopher.tigerlang.compiler.bind.Binder;
+import com.chaosopher.tigerlang.compiler.errormsg.ErrorMsg;
+import com.chaosopher.tigerlang.compiler.parse.CupParser;
+import com.chaosopher.tigerlang.compiler.parse.Parser;
+import com.chaosopher.tigerlang.compiler.parse.ParserFactory;
+import com.chaosopher.tigerlang.compiler.parse.ParserService;
+import com.chaosopher.tigerlang.compiler.parse.Program;
+
+
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private ParserService parserService;
+
+	public Application() {
+		parserService = new ParserService(new ParserFactory());
+	}
+
 	@GetMapping("/")
 	public Greeting greet(@RequestParam(value = "name", defaultValue = "David") String name) {
+		ErrorMsg errorMsg = new ErrorMsg("", System.out);
+        Absyn program = this.parserService.parse("\"foo\" = \"bar\"", errorMsg);
+        program.accept(new Binder(errorMsg));
+        Desugar absynCloner = new Desugar();
+		program.accept(absynCloner);
 		return new Greeting(1, name);
 	}
 
