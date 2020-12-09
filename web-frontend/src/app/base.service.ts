1ca6121ed0cbd888cad19dc7b79f3a113175435b
David Knott <davek@communicraft.com>
2020-12-09 14:07:41 +0000

    hooked up compiler to front end

---
+++ web-frontend/src/app/base.service.ts
@@ -0,0 +1,11 @@
+import { throwError } from 'rxjs';
+
+export abstract class BaseService {
+  constructor() { }
+
+  protected handleError(error: any) {
+    // In a real world app, we might send the error to remote logging infrastructure
+    // and reformat for user consumption
+    return throwError(error);
+  }
+}
---
+++ web-frontend/src/app/core/core.modules.ts
@@ -0,0 +1,30 @@
+import {
+    NgModule,
+    ModuleWithProviders,
+    Optional,
+    SkipSelf
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// What Angular recommends is to put all of our global services in a separated module,
+// called CoreModule, and import it ONLY in AppModule. This way is the same as providing
+// the services in AppModule directly!
+@NgModule({
+    declarations: [],
+    imports: [CommonModule]
+})
+export class CoreModule {
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        if (parentModule) {
+            throw new Error(
+                'CoreModule is already loaded. Import it in the AppModule only'
+            );
+        }
+    }
+
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: CoreModule
+        };
+    }
+}
