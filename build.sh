f91c7e0de7d9772798b9d87a5ba4d73a88805176
David Knott <davek@communicraft.com>
2020-12-19 18:53:12 +0000

    build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+mvn install
+cd web-backend
+mvn clean package
+docker image rm tiger-web-backend
+docker build -t tiger-web-backend .
+docker container rm -f tiger-web-backend-container
+docker container rm -f tiger-web-frontend-container
+docker run --rm -d --name tiger-backend-container  -p 10000:8080 tiger-web-backend
+cd ../web-frontend
+docker image rm tiger-front-end:latest
+docker build -t tiger-front-end .
+docker run --rm -d --name tiger-frontend-container  -p 4200:80 tiger-front-en
