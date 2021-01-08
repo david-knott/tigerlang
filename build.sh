5b531edae375c0078a5be8febb3591ebc937825d
David Knott <davek@communicraft.com>
2021-01-08 19:03:07 +0000

    build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd web-backend
 mvn clean package
 docker container rm -f tiger-web-backend-container
 docker build -t tiger-web-backend .
-docker run --rm -d --name tiger-web-backend-container  -p 10000:8080 tiger-web-backend
+docker run --rm -d --name tiger-web-backend-container  -p 8080:8080 tiger-web-backend
 cd ../web-frontend
 docker container rm -f tiger-web-frontend-container
 docker build -t tiger-front-end .
