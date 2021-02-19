d31eac71e3c6036c8dca8f4c6399f44d307ea3fe
David Knott <mrdavidknott@gmail.com>
2021-02-19 13:19:53 +0000

    added missing files

---
+++ compiler/src/main/java/com/chaosopher/tigerlang/compiler/target/Tasks.java
@@ -0,0 +1,16 @@
+package com.chaosopher.tigerlang.compiler.target;
+
+import com.chaosopher.tigerlang.compiler.util.DisjunctiveTask;
+import com.chaosopher.tigerlang.compiler.util.SimpleTask;
+import com.chaosopher.tigerlang.compiler.util.SimpleTaskProvider;
+import com.chaosopher.tigerlang.compiler.util.TaskContext;
+import com.chaosopher.tigerlang.compiler.util.TaskProvider;
+import com.chaosopher.tigerlang.compiler.util.TaskRegister;
+
+public class Tasks implements TaskProvider {
+
+    @Override
+    public void build(TaskRegister taskRegister) {
+
+    }
+}
